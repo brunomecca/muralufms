2d2f150274ecff5626f7707bdbc8965daa46601e
Bruno Mecca <brunogmecca@gmail.com>
2017-03-25 16:33:20 -0400

    ae

--- index2.php
+++ index2.php
@@ -1,4 +1,6 @@
 <?php
+  require_once("model/MensagemDAO.php");
+  require_once("model/UsuarioDAO.php");
   if(!isset($_GET['page']) || $_GET['page'] == ''){
     include('controllers/HomeControle2.php');
   }
