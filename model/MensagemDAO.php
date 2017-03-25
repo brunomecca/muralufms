390cff08c2547c5d35deba19e028ee9e218aeee1
Bruno Mecca <brunogmecca@gmail.com>
2017-03-25 15:56:10 -0400

    ae
    ae

---
+++ model/MensagemDAO.php
@@ -0,0 +1,23 @@
+<?php
+	include "connect.php";
+	class MensagemDAO{
+		function adicionarNoBanco($mensagem){
+			$titulo = $mensagem.getTitulo();
+			$mensagem = $mensagem.getMensagem();
+			$id_usuario = $mensagem.getIdUsuario();
+			$opiniao = $mensagem.getOpiniao();
+			$positivo = 0;
+			$negativo = 0;
+
+			$inseresql = mysqli_query($link, "INSERT INTO mensagens (id_usuario, titulo, mensagem, opiniao, positivo, negativo) VALUES ('$id_usuario', '$titulo', '$mensagem', '$opiniao','$positivo','$negativo')");
+			if($inseresql == True)
+				return True;
+			else
+				return False;
+		}
+
+		function puxarDoBanco(){
+			$consulta = mysqli_query($link, "SELECT * FROM mensagens ORDER BY id DESC");
+		}
+	}
+?>
